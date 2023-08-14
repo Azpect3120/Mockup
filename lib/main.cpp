9cbdbbf88f274c9da27c4c6ffb6819a68d66f63c
Azpect3120 <hhargreaves2006@gmail.com>
2023-08-13 22:00:47 -0700

    Added command path feature, now requires path in the .exe call

--- lib/main.cpp
+++ lib/main.cpp
@@ -9,24 +9,23 @@ using std::vector;
 
 int main (int argc, char* argv[])
 {
-    // Print args given to the exe file
-    /*
-    for (int i = 0; i < argc; i++) {
-        std::cout << argv[i] << std::endl;
-    } 
-    */       
+   if (argc != 2) {
+    std::cerr << "Please provide a path." << std::endl;
+    return 1;
 
-    // Create compiler object with this current path
-    Compiler compiler;
+   } else {
+        Compiler compiler; // Create compiler object with this current path
 
-    compiler.setInputPath("../../MockUp/test.mock");
-    compiler.setOutputPath("../../MockUp/output.html");
+        compiler.setInputPath(argv[1]); // Set the input file to the user defined path
 
-    vector<string> inputLineData = compiler.readInput(); 
+        // Convert user defined path to output file
+        string outputPath = argv[1];
+        outputPath.replace(outputPath.length() - 4, 4, "html");
 
-    vector<string> interpretedData = compiler.interpretLineData(inputLineData);
-
-    compiler.writeOutput(interpretedData);
-
+        compiler.setOutputPath(outputPath); // Set the output path
+        vector<string> inputLineData = compiler.readInput(); // Read data from input file 
+        vector<string> interpretedData = compiler.interpretLineData(inputLineData); // Compile input data
+        compiler.writeOutput(interpretedData); // Write compiled data into output file.
+   }
     return 0;
 }
