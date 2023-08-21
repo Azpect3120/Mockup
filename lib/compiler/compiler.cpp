9fa050600f74948c933ddded6cd8384a601c5f74
Azpect3120 <hhargreaves2006@gmail.com>
2023-08-21 10:35:03 -0700

    Blank lines cause crash.

--- lib/compiler/compiler.cpp
+++ lib/compiler/compiler.cpp
@@ -53,10 +53,10 @@ void compileNode (TreeNode* node, std::ofstream& outputFile)
             int indentCount = countIndentLevel(node->data) - 1;
             for (int i = 0; i < indentCount; i++) {outputFile << "    ";}
             outputFile << "</div>" << std::endl;
-        }
+        
 
         // UL tag
-        else if (node->data.find("UL") != string::npos) {
+        } else if (node->data.find("UL") != string::npos) {
             node->data.replace(node->data.find("UL"), 2, "<ul>");
             
             // Write node data
@@ -73,10 +73,10 @@ void compileNode (TreeNode* node, std::ofstream& outputFile)
             int indentCount = countIndentLevel(node->data) - 1;
             for (int i = 0; i < indentCount; i++) {outputFile << "    ";}  
             outputFile << "</ul>" << std::endl;
-        }
+        
 
         // OL tag
-        else if (node->data.find("OL") != string::npos) {
+        } else if (node->data.find("OL") != string::npos) {
             node->data.replace(node->data.find("OL"), 2, "<ol>");
             
             // Write node data
@@ -94,6 +94,50 @@ void compileNode (TreeNode* node, std::ofstream& outputFile)
             for (int i = 0; i < indentCount; i++) {outputFile << "    ";}
             outputFile << "</ol>" << std::endl;
 
+        // BODY tag
+        } else if (node->data.find("BODY") != string::npos) { 
+            node->data.replace(node->data.find("BODY"), 4, "<body>");
+
+            // Write node data
+            outputFile << node->data << std::endl;
+            
+            // Write nodes childrens data
+            for (TreeNode* child : node->children) {
+                compileNode(child, outputFile);
+            }
+
+            // Write nodes closing data
+            int indentCount = countIndentLevel(node->data) - 1;
+            for (int i = 0; i < indentCount; i++) {outputFile << "    ";}
+            outputFile << "</body>" << std::endl;
+
+        
+        // HEAD tag
+        } else if (node->data.find("HEAD") != string::npos) {
+            node->data.replace(node->data.find("HEAD"), 4, "<head>");
+
+            // Write node data
+            outputFile << node->data << std::endl;
+
+            // Determine indent count
+            int indentCount = countIndentLevel(node->data);
+
+            // Add standard meta-data
+            for (int i = 0; i < indentCount; i++) {outputFile << "    ";}
+            outputFile << "<meta charset=\"UTF-8\">" << std::endl;
+
+            for (int i = 0; i < indentCount; i++) {outputFile << "    ";}
+            outputFile << "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">" << std::endl;            
+            
+            // Write nodes childrens data
+            for (TreeNode* child : node->children) {
+                compileNode(child, outputFile);
+            }
+
+            // Write nodes closing data
+            for (int i = 1; i < indentCount; i++) {outputFile << "    ";}
+            outputFile << "</head>" << std::endl;
+
         // Line was not handled with other syntax
         } else {
             outputFile << node->data << std::endl;
@@ -101,6 +145,49 @@ void compileNode (TreeNode* node, std::ofstream& outputFile)
             for (TreeNode* child : node->children) {
                 compileNode(child, outputFile);
             }
+        }
+
+    // Check if first character is the inline character ($)
+    } else if (node->data.at(node->data.find_first_not_of(' ')) == '$') {
+        // Remove inline character ($)
+        node->data.erase(node->data.find_first_of('$'), 1);
+
+        // TITLE tag
+        if (node->data.find("TITLE") != string::npos) {
+            // Remove title tag
+            node->data.replace(node->data.find("TITLE"), 5, "<title>");
+
+            // Add closing tag
+            node->data.append("</title>");
+
+            // Write node data
+            outputFile << node->data << std::endl;
+
+        // STYLE tag
+        } else if (node->data.find("STYLE") != string::npos) {
+            // Remove style tag
+            node->data.replace(node->data.find("STYLE"), 6, "<link rel=\"stylesheet\" href=\"");
+
+            // Add closing tag
+            node->data.append("\">");
+
+            // Write node data
+            outputFile << node->data << std::endl;
+
+        // SCRIPT tag
+        } else if (node->data.find("SCRIPT") != string::npos) {
+            // Remove script tag
+            node->data.replace(node->data.find("SCRIPT"), 7, "<script src=\"");
+
+            // Add closing tag
+            node->data.append("\"></script>");
+
+            // Write node data
+            outputFile << node->data << std::endl;
+
+        // Unhandled 
+        } else {
+            outputFile << node->data << std::endl;
         }
 
     // Check if first character is a heading (#<1-6>)
