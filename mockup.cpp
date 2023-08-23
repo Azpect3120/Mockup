beff888558846dd4cdc38961cad87ac1e4e53c5c
Azpect3120 <hhargreaves2006@gmail.com>
2023-08-23 11:23:10 -0700

    Added time modules to save the output files using current timestamp.

--- mockup.cpp
+++ mockup.cpp
@@ -3,6 +3,11 @@
 #include <vector>
 #include <fstream>
 
+#include <chrono>
+#include <ctime>
+#include <sstream>
+#include <iomanip>
+
 #include "./lib/compiler/compiler.h"
 #include "./lib/scopeTree/scopeTree.h"
 #include "./lib/scopeTree/treeNode.h"
@@ -20,10 +25,22 @@ int main ()
     // std::cout << "Raw tree:" << std::endl;
     tree.printTree(tree.root);
 
+    // Get current time and convert to string
+    auto now = std::chrono::system_clock::now();
+    std::time_t now_c = std::chrono::system_clock::to_time_t(now);
+
+    std::tm* ptm = std::localtime(&now_c);
+    std::ostringstream oss;
+    oss << std::put_time(ptm, "%m-%d(%H:%M:%S)");
+    std::string timestamp = oss.str();
+
+    // Create output path from timestamp
+    std::string outputPath = "./build/output" + timestamp + ".html";
+
     // Compile tree
     // Writes to file
     std::cout << "Compiling..." << std::endl;
-    compileTree(tree.root, "./build/output.html");
+    compileTree(tree.root, outputPath);
     std::cout << "Compilation complete!" << std::endl;
 
     return 0;
